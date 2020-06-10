3a1f957e9a6c15458c1fb8a2018e43a0838f3c55
mcatzin <catzin21@gmail.com>
2020-06-10 10:17:06 -0700

    Project status Pie chart

--- src/main/java/com/mc/pma/controllers/HomeController.java
+++ src/main/java/com/mc/pma/controllers/HomeController.java
@@ -1,16 +1,20 @@
 package com.mc.pma.controllers;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mc.pma.dao.EmployeeRepository;
 import com.mc.pma.dao.ProjectRepository;
+import com.mc.pma.dto.ChartData;
 import com.mc.pma.dto.EmployeeProject;
-import com.mc.pma.entities.Employee;
 import com.mc.pma.entities.Project;
 
 @Controller
@@ -22,10 +26,23 @@ public class HomeController {
 	EmployeeRepository empRepo;
 	
 	@GetMapping("/")
-	public String displayHome(Model model) {
+	public String displayHome(Model model) throws JsonProcessingException {
+		
+		Map<String, Object> map = new HashMap<>();
+		
+		// we are querying the database for projects
 		List<Project> projects = proRepo.findAll();
 		model.addAttribute("projectList", projects);
 		
+		List<ChartData> projectData = proRepo.getProjectStatus();
+		
+		// Lets convert projectData object into json structure for use in JavaScript
+		ObjectMapper objectMapper = new ObjectMapper();
+		String jsonString = objectMapper.writeValueAsString(projectData);
+		
+		model.addAttribute("projectStatusCnt", jsonString);
+		
+		// we are querying the database for employees
 		List<EmployeeProject> employeesProjectCnt = empRepo.employeeProjects();
 		model.addAttribute("employeeListProjectCnt", employeesProjectCnt);
 		
--- src/main/resources/static/js/myChart.js
+++ src/main/resources/static/js/myChart.js
@@ -1,16 +1,40 @@
- new Chart(document.getElementById("myPieChart"), {
+const chartDataStr = decodeHtml(chartData); 
+
+const chartJsonArray = JSON.parse(chartDataStr);
+
+const arrayLength = chartJsonArray.length;
+const numericData = [];
+const labelData= [];
+
+for(let i = 0;i< arrayLength;i++){
+	numericData[i] = chartJsonArray[i].value;
+	labelData[i] = chartJsonArray[i].label;
+}
+
+new Chart(document.getElementById("myPieChart"), {
     type: 'pie',
     // The data for our dataset
     data: {
-        labels: ['January', 'February', 'March'],
+        labels: labelData,
         datasets: [{
             label: 'My First dataset',
             backgroundColor: ["#3e95cd","#8e5ea2","#3cba9f"],
-            borderColor: 'rgb(255, 99, 132)',
-            data: [5, 10, 5,]
+            data: numericData,
         }]
     },
 
     // Configuration options go here
-    options: {}
+    options: {
+    	title: {
+    		display: true,
+    		text: "Project Statuses"
+    	}
+    }
 });
+ 
+ 
+ function decodeHtml(html){
+	 const txt = document.createElement("textarea");
+	 txt.innerHTML = html;
+	 return txt.value;
+ }
